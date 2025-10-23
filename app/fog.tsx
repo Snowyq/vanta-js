56148e6798ee3495d4912cc5622a4f0ea5b354e7
Jakub Pniewski <jakub.pniewski@icloud.com>
2025-10-23 10:43:44 +0200

    frist commit

---
+++ app/fog.tsx
@@ -0,0 +1,48 @@
+"use client";
+import { useEffect, useRef, useState } from "react";
+import { VANTA } from "../lib/vanta-js/vanta.fog";
+import * as THREE from "three";
+import * as motion from "motion/react-client";
+import { useMotionValueEvent, useScroll } from "motion/react";
+
+export default function Fog() {
+  const ref = useRef<HTMLDivElement | null>(null);
+  const fogRef = useRef();
+  const { scrollYProgress } = useScroll({
+    target: ref,
+    offset: ["start start", "end start"],
+  });
+
+  useEffect(() => {
+    if (!ref.current) return;
+
+    const fog = VANTA.FOG({
+      THREE: THREE,
+      el: ref.current,
+      mouseControls: true,
+      touchControls: true,
+      gyroControls: false,
+      minHeight: 200.0,
+      minWidth: 200.0,
+      highlightColor: 0xdedede,
+      midtoneColor: 0xd97f6a,
+      lowlightColor: 0xa71938,
+      baseColor: 0xd22727,
+      blurFactor: 0.39,
+      speed: 1,
+      zoom: 2,
+    });
+
+    fogRef.current = fog;
+  }, [ref]);
+
+  useMotionValueEvent(scrollYProgress, "change", (latest) => {
+    console.log("Page scroll: ", latest);
+    if (fogRef.current) {
+      console.log(fogRef.current);
+      fogRef.current.setOptions({ speed: 1 + latest * 5, zoom: 2 - latest });
+    }
+  });
+
+  return <motion.div ref={ref} className="h-full w-full"></motion.div>;
+}
